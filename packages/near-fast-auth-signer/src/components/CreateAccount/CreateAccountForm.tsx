04a70da7cb412e7a36ff2c6cf7cba3af4a80b9e7
Charles Moore <charles@ozone.tech>
2024-10-18 09:02:37 -0400

    Adjustments to the styling of register

--- packages/near-fast-auth-signer/src/components/CreateAccount/CreateAccountForm.tsx
+++ packages/near-fast-auth-signer/src/components/CreateAccount/CreateAccountForm.tsx
@@ -1,7 +1,7 @@
 // CreateAccountForm.jsx
 import { yupResolver } from '@hookform/resolvers/yup';
-import React, { useEffect, forwardRef, useCallback } from 'react';
-import { useForm, SubmitHandler } from 'react-hook-form';
+import React, { forwardRef, useCallback, useEffect } from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
 import { Link, useSearchParams } from 'react-router-dom';
 import styled from 'styled-components';
 import isEmail from 'validator/lib/isEmail';
@@ -14,13 +14,15 @@ import { openToast } from '../../lib/Toast';
 import { recordEvent } from '../../utils/analytics';
 import { network } from '../../utils/config';
 import environment from '../../utils/environment';
-import { accountAddressPatternNoSubAccount, getEmailId } from '../../utils/form-validation';
+import {
+  accountAddressPatternNoSubAccount,
+  getEmailId,
+} from '../../utils/form-validation';
 import { FormContainer } from '../Layout';
 // eslint-disable-next-line import/no-cycle
-import SocialLogin from '../SocialLogin/SocialLogin';
 
 const CreateAccountFormContainer = styled(FormContainer)`
-  height: ${environment.NETWORK_ID === 'testnet' ? '645px;' : '500px;'}
+  height: ${environment.NETWORK_ID === 'testnet' ? '645px;' : '500px;'};
 `;
 
 const emailProviders = ['gmail', 'yahoo', 'outlook'];
@@ -106,148 +108,150 @@ type CreateAccountFormProps = {
   onSubmit: SubmitHandler<CreateAccountFormValues>;
   loading: boolean;
   initialValues?: CreateAccountFormValues;
-}
+};
 
 // eslint-disable-next-line max-len
-const CreateAccountForm = forwardRef<HTMLFormElement, CreateAccountFormProps>(({
-  onSubmit, loading, initialValues
-}, ref) => {
-  const [searchParams] = useSearchParams();
+const CreateAccountForm = forwardRef<HTMLFormElement, CreateAccountFormProps>(
+  ({ onSubmit, loading, initialValues }, ref) => {
+    const [searchParams] = useSearchParams();
 
-  const {
-    register,
-    handleSubmit,
-    setValue,
-    watch,
-    reset,
-    trigger,
-    formState: { errors, isValid },
-  } = useForm({
-    mode:          'all',
-    resolver:      yupResolver(schema),
-    defaultValues: initialValues ?? {
-      email:    '',
-      username: '',
-    },
-  });
+    const {
+      register,
+      handleSubmit,
+      setValue,
+      watch,
+      reset,
+      trigger,
+      formState: { errors, isValid },
+    } = useForm({
+      mode:          'all',
+      resolver:      yupResolver(schema),
+      defaultValues: initialValues ?? {
+        email:    '',
+        username: '',
+      },
+    });
 
-  const formsEmail = watch('email');
-  const formsUsername = watch('username');
+    const formsEmail = watch('email');
+    const formsUsername = watch('username');
 
-  useEffect(() => {
-    const email = searchParams.get('email');
-    const username = searchParams.get('accountId');
+    useEffect(() => {
+      const email = searchParams.get('email');
+      const username = searchParams.get('accountId');
 
-    if (email) {
-      reset({
-        email,
-        username: username || getEmailId(email),
-      });
-      trigger();
+      if (email) {
+        reset({
+          email,
+          username: username || getEmailId(email),
+        });
+        trigger();
 
-      if (username) {
-        handleSubmit(onSubmit)();
+        if (username) {
+          handleSubmit(onSubmit)();
+        }
       }
-    }
-  }, [onSubmit, handleSubmit, reset, searchParams, trigger]);
+    }, [onSubmit, handleSubmit, reset, searchParams, trigger]);
 
-  useEffect(() => {
-    if (formsEmail?.split('@').length > 1 && !formsUsername) {
-      setValue('username', getEmailId(formsEmail), { shouldValidate: true, shouldDirty: true });
-    }
-  }, [formsEmail, formsUsername, setValue]);
+    useEffect(() => {
+      if (formsEmail?.split('@').length > 1 && !formsUsername) {
+        setValue('username', getEmailId(formsEmail), {
+          shouldValidate: true,
+          shouldDirty:    true,
+        });
+      }
+    }, [formsEmail, formsUsername, setValue]);
 
-  const handleSignInClick = () => {
-    recordEvent('click-has-account-sign-in');
-  };
+    const handleSignInClick = () => {
+      recordEvent('click-has-account-sign-in');
+    };
 
-  const loginQueryParam = useCallback(() => {
-    searchParams.set('isRecovery', 'true');
-    return searchParams.toString();
-  }, [searchParams]);
+    const loginQueryParam = useCallback(() => {
+      searchParams.set('isRecovery', 'true');
+      return searchParams.toString();
+    }, [searchParams]);
 
-  return (
-    <CreateAccountFormContainer ref={ref} onSubmit={handleSubmit(onSubmit)}>
-      <header>
-        <h1 data-test-id="heading_create">Create account</h1>
-        <p className="desc">
-          <span>Have an account?</span>
-          {' '}
-          <Link
-            to={{ pathname: '/login', search: loginQueryParam() }}
-            data-test-id="create_login_link"
-            onClick={handleSignInClick}
-          >
-            Sign in
-          </Link>
-        </p>
-      </header>
-      <Input
-        {...register('email')}
-        debounceTime={1000}
-        placeholder="user_name@email.com"
-        type="email"
-        label="Email"
-        error={errors?.email?.message}
-        badges={emailProviders?.reduce((acc, provider) => {
-          const username = formsEmail?.split('@')[0];
-          const currProvider = formsEmail?.split('@')[1];
+    return (
+      <CreateAccountFormContainer ref={ref} onSubmit={handleSubmit(onSubmit)}>
+        <header>
+          <h1 data-test-id="heading_create">Create account</h1>
+          <p className="desc">
+            <span>Have an account?</span>
+            {' '}
+            <Link
+              to={{ pathname: '/login', search: loginQueryParam() }}
+              data-test-id="create_login_link"
+              onClick={handleSignInClick}
+            >
+              Sign in
+            </Link>
+          </p>
+        </header>
+        <Input
+          {...register('email')}
+          debounceTime={1000}
+          placeholder="user_name@email.com"
+          type="email"
+          label="Email"
+          error={errors?.email?.message}
+          badges={emailProviders?.reduce((acc, provider) => {
+            const username = formsEmail?.split('@')[0];
+            const currProvider = formsEmail?.split('@')[1];
 
-          if (currProvider?.includes(provider)) {
+            if (currProvider?.includes(provider)) {
+              return [
+                {
+                  isSelected: true,
+                  label:      `@${provider}`,
+                  onClick:    () => setValue('email', username, { shouldValidate: true }),
+                },
+              ];
+            }
+
+            if (acc.some((p) => p.isSelected)) return acc;
+
             return [
+              ...acc,
               {
-                isSelected: true,
+                isSelected: false,
                 label:      `@${provider}`,
-                onClick:    () => setValue('email', username, { shouldValidate: true }),
+                onClick:    () => setValue('email', `${username}@${provider}.com`, {
+                  shouldValidate: true,
+                }),
               },
             ];
-          }
-
-          if (acc.some((p) => p.isSelected)) return acc;
-
-          return [
-            ...acc,
-            {
-              isSelected: false,
-              label:      `@${provider}`,
-              onClick:    () => setValue('email', `${username}@${provider}.com`, {
-                shouldValidate: true,
-              }),
-            },
-          ];
-        }, [])}
-        dataTest={{
-          input: 'email_create',
-          error: 'create_email_subtext_error',
-        }}
-      />
-      <Input
-        {...register('username')}
-        debounceTime={1000}
-        label="Account ID"
-        success={!errors.username && formsUsername && 'Account ID available'}
-        error={errors?.username?.message}
-        subText="Use a suggested ID or customize your own"
-        autoComplete="webauthn username"
-        right={`.${network.fastAuth.accountIdSuffix}`}
-        placeholder="user_name"
-        dataTest={{
-          input:   'username_create',
-          error:   'account_available_notice',
-          success: 'create-error-subtext',
-        }}
-      />
-      <Button
-        disabled={!isValid || loading}
-        label={loading ? 'Please wait...' : 'Continue'}
-        variant="affirmative"
-        type="submit"
-        size="large"
-        data-test-id="continue_button_create"
-      />
-      <SocialLogin />
-    </CreateAccountFormContainer>
-  );
-});
+          }, [])}
+          dataTest={{
+            input: 'email_create',
+            error: 'create_email_subtext_error',
+          }}
+        />
+        <Input
+          {...register('username')}
+          debounceTime={1000}
+          label="Account ID"
+          success={!errors.username && formsUsername && 'Account ID available'}
+          error={errors?.username?.message}
+          subText="Use a suggested ID or customize your own"
+          autoComplete="webauthn username"
+          right={`.${network.fastAuth.accountIdSuffix}`}
+          placeholder="user_name"
+          dataTest={{
+            input:   'username_create',
+            error:   'account_available_notice',
+            success: 'create-error-subtext',
+          }}
+        />
+        <Button
+          disabled={!isValid || loading}
+          label={loading ? 'Please wait...' : 'Continue'}
+          variant="affirmative"
+          type="submit"
+          size="large"
+          data-test-id="continue_button_create"
+        />
+      </CreateAccountFormContainer>
+    );
+  }
+);
 
 export default CreateAccountForm;
